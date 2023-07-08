51b8e6d7a9aeb2f91e6a3dfe043c45d2ae74c3f9
alokkusingh <alok.ku.singh@gmail.com>
2023-07-08 19:53:04 +0530

    Added AbcDataService call Server side streaming

--- src/main/java/com/alok/grpc/GrpcClient.java
+++ src/main/java/com/alok/grpc/GrpcClient.java
@@ -1,9 +1,20 @@
 package com.alok.grpc;
 
+import com.alok.grpc.stub.AbcDataServiceGrpc;
+import com.alok.grpc.stub.AbcDataServiceOuterClass;
+import com.alok.grpc.stub.AbcDataServiceOuterClass.AbcDataRequest;
+import com.alok.grpc.stub.AbcDataServiceOuterClass.AbcDataResponse;
 import com.alok.grpc.stub.UserGrpc;
-import com.alok.grpc.stub.UserOuterClass;
+import com.alok.grpc.stub.UserOuterClass.LoginRequest;
+import com.alok.grpc.stub.UserOuterClass.APIResponse;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.Iterator;
+import java.util.UUID;
 
 public class GrpcClient {
 
@@ -11,14 +22,42 @@ public class GrpcClient {
 
         ManagedChannel channel = ManagedChannelBuilder.forAddress("127.0.0.1", 9090).usePlaintext().build();
 
-        UserGrpc.UserBlockingStub stub = UserGrpc.newBlockingStub(channel);
+        login(channel);
 
-        UserOuterClass.LoginRequest loginRequest = UserOuterClass.LoginRequest.newBuilder()
+        getAbcData(channel);
+    }
+
+    public static void login(ManagedChannel channel) {
+        LoginRequest loginRequest = LoginRequest.newBuilder()
                 .setUsername("Alok")
                 .setPassword("Alok")
                 .build();
 
-        UserOuterClass.APIResponse loginResponse = stub.login(loginRequest);
+        UserGrpc.UserBlockingStub stub = UserGrpc.newBlockingStub(channel);
+        APIResponse loginResponse = stub.login(loginRequest);
         System.out.println(loginResponse);
+    }
+
+    public static void getAbcData(ManagedChannel channel) {
+
+        AbcDataRequest request = AbcDataRequest.newBuilder()
+                .setId(UUID.randomUUID().toString())
+                .setMonth(YearMonth.now().toString())
+                .build();
+
+
+        AbcDataServiceGrpc.AbcDataServiceBlockingStub blockingStub = AbcDataServiceGrpc.newBlockingStub(channel);
+        Iterator<AbcDataResponse> abcDataItr;
+        try {
+            System.out.println("REQUEST::getAbcData - " + request);
+            abcDataItr = blockingStub.getAbcData(request);
+            for (int i = 1; abcDataItr.hasNext(); i++) {
+                AbcDataResponse abcData = abcDataItr.next();
+                System.out.println("RESPONSE::getAbcData - Price #" + i + ": " +  abcData);
+            }
+        } catch (StatusRuntimeException e) {
+            System.out.println("RPC failed: " +  e.getStatus());
+        }
+
     }
 }
